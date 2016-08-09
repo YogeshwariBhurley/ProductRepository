dfeb14d9ebbd368778f33658dad61e4f9a7c45e5
Yogeshwari Bhurley <yogeshwaribhurley@gmail.com>
2016-08-09 16:13:28 +0530

    Files for Product,ProductCategory,Customer

---
+++ ProductApp/ProductApp/Controllers/CustomerController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ProductApp.Context;
+using ProductApp.Models;
+using System.Net;
+using System.Data.Entity;
+
+namespace ProductApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        ProductContext db = new ProductContext();
+        public ActionResult Index()
+        {
+            return View(db.Customer_Context.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customers objCustomers)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Customer_Context.Add(objCustomers);
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(objCustomers);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customer_Context.Find(id);
+
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+        [HttpPost]
+        public ActionResult Edit([Bind(Exclude = "DateCreated,DateUpdated")] Customers objCustomers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(objCustomers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(objCustomers);
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Customers customer = db.Customer_Context.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customer = db.Customer_Context.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customers customer = db.Customer_Context.Find(id);
+            db.Customer_Context.Remove(customer);
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProductApp/ProductApp/Controllers/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ProductApp.Models;
+using ProductApp.Context;
+
+namespace ProductApp.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: Transaction
+        ProductContext db = new ProductContext();
+        public ActionResult Index()
+        {
+            return View(db.PurchaseTransactionSummery_Context.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
