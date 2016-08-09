cc04648fc78dfe77e6f4dfcd9af95b0d6bea2964
Yogeshwari Bhurley <yogeshwaribhurley@gmail.com>
2016-08-09 18:17:47 +0530

    added source files

---
+++ ProductApp/Context/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using System.Data.Entity;
+using ProductApp.Models;
+
+namespace ProductApp.Context
+{
+    public class ProductContext:DbContext
+    {
+        public DbSet<Products> Product_Context { get; set; }
+        public DbSet<ProductCategories> ProductCategories_Context { get; set; }
+        public DbSet<Customers> Customer_Context { get; set; }
+        public DbSet<PurchaseTransactionSummeries> PurchaseTransactionSummery_Context { get; set; }
+        public DbSet<PurchaseTransactionDetails> PurchaseTransactionDetails_Context { get; set; }
+
+    }
+}
